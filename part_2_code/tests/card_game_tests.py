9cf38369525a80c4bdcf3436a6dfc2cd61518039
lushpurr <lushpurr@gmail.com>
2020-11-13 16:31:23 +0000

    realised I was doing this wrong. refilled errors in card_game.py. Test 1 written

--- part_2_code/tests/card_game_tests.py
+++ part_2_code/tests/card_game_tests.py
@@ -7,4 +7,7 @@ class TestCardGame(unittest.TestCase):
         self.card = Card()
         self.card_game = CardGame(self.card)
     
-    
+    def test_check_for_ace_true(self):
+        card1 = Card("hearts", 1)
+        result = CardGame.check_for_ace(self, card1)
+        selfAssertEqual(True, result)
