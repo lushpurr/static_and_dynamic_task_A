6924911e2562c00f3afcd94985a13afee810af8d
lushpurr <lushpurr@gmail.com>
2020-11-13 16:38:09 +0000

    test 3 written

--- part_2_code/tests/card_game_tests.py
+++ part_2_code/tests/card_game_tests.py
@@ -12,4 +12,21 @@ class TestCardGame(unittest.TestCase):
         result = CardGame.check_for_ace(self, card1)
         self.AssertEqual(True, result)
 
+    def test_check_for_ace_false(self):
+        card1 = Card("hearts", 2)
+        result = CardGame.check_for_ace(self, card1)
+        self.AssertEqual(False, result)
+
+    def test_highest_card_card1(self):
+        card1 = Card("hearts", 10)
+        card2 = Card("spades", 2)
+        result = CardGame.check_for_ace(self, card1, card2)
+        self.AssertEqual(card1, result)
+        
+
+    def test_highest_card_card2(self):
+
+
+    def test_card_total(self):
+
     
